33eef72f52977ae3d8fc0120b5f36fb87c29f9cb
Margiris <34870816+Margiris@users.noreply.github.com>
2019-10-30 17:59:42 +0200

    remove hello world stuff, add url checking example

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from api import CatApi
-from classes import HelloWorld, Multi, User
+from classes import User
 
 
 app = Flask(__name__)
@@ -10,8 +10,6 @@ db = SQLAlchemy(app)
 
 api = CatApi(app, catch_all_404s=True)
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Multi, '/multi')
 api.add_resource(User, '/user')
 
 if __name__ == "__main__":
--- classes.py
+++ classes.py
@@ -1,25 +1,28 @@
 from flask import Flask, request
 from flask_restful import Resource
 from flask_sqlalchemy import SQLAlchemy
+from app import db
+
+# class Team(Resource):
+#     def post(self, team_id=None, player_id=None):
+#         if team_id is None and player_id is None:
+#             # first version
+#         if team_id is not None and player_id is None:
+#             # second version
+#         if team_id is not None and player_id is not None:
+#             # third version
 
 
-class HelloWorld(Resource):
+class User(db.Model, Resource):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+
     def get(self):
-        return {'about': 'Hi World'}
+        num = int(request.args['number'])
+        return {}
 
     def post(self):
         received_data = request.get_json()
         print(received_data)
         return {'received': received_data}, 201
-
-
-class Multi(Resource):
-    def get(self):
-        print(request.args)
-        num = int(request.args['number'])
-        return {'result': num*8}
-
-
-class User(Resource):
-    def get(self):
-        return {}
