32b699f34653705bc8ab96847c2e8edbceebd947
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 18:37:02 +0200

    Update catpic_server_start.sh

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -3,11 +3,15 @@
 # go to project dir
 cd ~/catpic
 
+#  drop any changes
+git stash save --keep-index
+git stash drop
+
 # update dev branch
 git checkout dev
 git pull
 
-# if scripts differ we assume that it was updated and copy new script in place of older and exec it
+# if scripts differ we assume that it was updated and copy new script in place of older, then chmod and exec it
 cmp ~/.termux/tasker/catpic_server_start.sh ./scripts/catpic_server_start.sh || cp ./scripts/catpic_server_start.sh ~/.termux/tasker/ && chmod 700 && exec ~/.termux/tasker/catpic_server_start.sh
 
 which python
