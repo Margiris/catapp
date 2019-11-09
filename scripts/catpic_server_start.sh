9f2b08d3907e0b294253a6b41a58c44fabb82c94
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 23:12:48 +0200

    automation script: stylistical changes

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -4,38 +4,38 @@
 cd ~/catpic
 
 #  drop any changes
-echo 'droping any changes...'
+echo '\nDroping any changes...\n'
 git stash save --keep-index
 git stash drop
 
 # update dev branch
-echo 'updating dev branch...'
+echo '\nUpdating dev branch...\n'
 git checkout dev
 git pull
 
 # if scripts differ we assume that it was updated
-echo 'checking for a new script version...'
+echo '\nChecking for a new script version...\n'
 if cmp -s ~/.termux/tasker/catpic_server_start.sh ./scripts/catpic_server_start.sh;
 then
-    echo 'no new version found, continuing'
+    echo '\nNo new version found, continuing\n'
 else
     # copy new script in place of older one then exec it
-    echo 'found new version, updating...'
+    echo '\nFound new version, updating...\n'
     cp ./scripts/catpic_server_start.sh ~/.termux/tasker/catpic_server_start.sh
-    echo 'restarting script...'
+    echo '\nRestarting script...\n'
     exec ~/.termux/tasker/catpic_server_start.sh
 fi
 
 # activate virtual environment
-echo 'activating virtual environment...'
+echo '\nActivating virtual environment...\n'
 source ./venv/bin/activate
 
 # update dependencies
-echo 'updating dependencies...'
+echo '\nUpdating dependencies...\n'
 pip install -r ./requirements.txt
 
 # kill any processes that might be occupying 8080 port
-echo 'freeing up required port...'
+echo '\nFreeing up required port...\n'
 kill $(netstat -ltnp | grep ':8080' | awk '{ print $7 }' | sed 's/[^0-9]*//g')
 
 #  run server
