b11f16fec25689b8443b6845d5225e37fe8e21e9
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 18:25:46 +0200

    Update catpic_server_start.sh

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# go to project dir
 cd ~/catpic
 
+# update dev branch
 git checkout dev
 git pull
 
-cp ./scripts/catpic_server_start.sh ~/.termux/tasker/
+# if scripts differ we assume that it was updated and copy new script in place of older and exec it 
+cmp ~/.termux/tasker/catpic_server_start.sh ./scripts/catpic_server_start.sh
+|| cp ./scripts/catpic_server_start.sh ~/.termux/tasker/
+&& exec ~/.termux/tasker/catpic_server_start.sh
 
+which python
 source ./venv/bin/activate
+which python
+
 if [$(which python) = "/data/data/com.termux/files/home/catpic/venv/bin/python"] then
     pip install -r ./requirements.txt
 else
