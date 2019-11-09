80c27151de9265cebf79135820f37b4edb92899d
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 17:22:28 +0200

    Update catpic_server_start.sh
    make autostart script update itself

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 cd ~/catpic
+
 git checkout dev
 git pull
+
+cp ./scripts/catpic_server_start.sh ~/.termux/tasker/
+
 source ./venv/bin/activate
+
 pip install -r ./requirements.txt
-python server.py
+
+python ./server.py
