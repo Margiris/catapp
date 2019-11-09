80192f3b66b74ae51f4496f2c04820caa9dc08d1
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 18:42:53 +0200

    Update catpic_server_start.sh
    fix syntax

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -15,10 +15,12 @@ git pull
 cmp ~/.termux/tasker/catpic_server_start.sh ./scripts/catpic_server_start.sh || cp ./scripts/catpic_server_start.sh ~/.termux/tasker/ && chmod 700 && exec ~/.termux/tasker/catpic_server_start.sh
 
 which python
+# activate virtual environment
 source ./venv/bin/activate
 which python
 
-if [$(which python) = "/data/data/com.termux/files/home/catpic/venv/bin/python"] then
+if [ $(which python) = "/data/data/com.termux/files/home/catpic/venv/bin/python" ]
+then
     pip install -r ./requirements.txt
 else
     echo $(which python)
