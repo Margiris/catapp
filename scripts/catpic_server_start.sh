8a1367e25b96fdfe0192411356d620996ee36c3f
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-09 23:45:43 +0200

    automation script: stylistical changes, test

--- scripts/catpic_server_start.sh
+++ scripts/catpic_server_start.sh
@@ -1,5 +1,6 @@
 # setup variables
 green_text='\e[0;32m%s\n\e[m'
+red_text='\e[0;31m%s\n\e[m'
 
 # go to project dir
 cd ~/catpic
@@ -15,36 +16,38 @@ git checkout dev
 git pull
 
 # if scripts differ we assume that it was updated
-printf $green_text 'Checking for a new script version...\n'
+printf $green_text 'Checking for a new script version...'
 if cmp -s ~/.termux/tasker/catpic_server_start.sh ./scripts/catpic_server_start.sh;
 then
-    printf $green_text 'No new version found, continuing\n'
+    printf $green_text 'No new version found, continuing'
 else
     # copy new script in place of older one then exec it
-    printf $green_text 'Found new version, updating...\n'
+    printf $green_text 'Found new version, updating...'
     cp ./scripts/catpic_server_start.sh ~/.termux/tasker/catpic_server_start.sh
     printf $green_text 'Restarting script...\n'
     exec ~/.termux/tasker/catpic_server_start.sh
 fi
 
 # activate virtual environment
-printf $green_text 'Activating virtual environment...\n'
-source ./venv/bin/activate
+# printf $green_text 'Activating virtual environment...'
+# source ./venv/bin/activate
 
 # check if virtual environment activation successful
 if [ $(which python) = "/data/data/com.termux/files/home/catpic/venv/bin/python" ]
 then
     # update dependencies
-    printf $green_text 'Updating dependencies...\n'
+    printf $green_text 'Updating dependencies...'
     pip install -r ./requirements.txt
     
     # kill any processes that might be occupying 8080 port
-    printf $green_text 'Freeing up required port...\n'
+    printf $green_text 'Freeing up required port...'
     kill $(netstat -ltnp | grep ':8080' | awk '{ print $7 }' | sed 's/[^0-9]*//g')
     
     #  run server
     python ./server.py
 else
-    echo $(which python)
+    printf $red_text 'Virtual environment activation failed, current python executable:'
+    printf $red_text $(which python)
+    printf $red_text 'Please restart.'
 fi
 
