b11a63dabed9d809bd1efa56ef7f28e9d8847e7c
Margiris <34870816+Margiris@users.noreply.github.com>
2019-12-05 22:30:38 +0200

    setup script: finish

--- scripts/catpic_server_setup.sh
+++ scripts/catpic_server_setup.sh
@@ -1,9 +1,23 @@
+# copy itself to required dir and restart
+if [ $(pwd) != '/data/data/com.termux/files/home/tasker' ];
+then
+    mv ./catpic_server_setup.sh /data/data/com.termux/files/home/
+    mkdir /data/data/com.termux/files/home/tasker > NUL
+    mv /data/data/com.termux/files/home/catpic_server_setup.sh /data/data/com.termux/files/home/tasker/
+    exec ~/tasker/catpic_server_setup.sh
+fi
+
 # setup variables
 green_text='\e[0;32m%s\n\e[m'
 
-# install dependencies
+# update system
+apt update
+apt upgrade
+# install dependencies separately to work around missing packages
 printf $green_text 'Installing dependencies...'
-pkg install git ncurses-utils python libjpeg-turbo
+for i in git ncurses-utils python libjpeg-turbo clang ndk-root libcrypt zlib; do
+  apt install $i
+done
 
 # download and setup sudo
 printf $green_text 'Setting up sudo...'
@@ -14,6 +28,13 @@ cat ~/termux-sudo/sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
 rm -fR ~/termux-sudo
+
+# wait for root permission if needed
+printf $green_text 'Waiting for root permission...'
+sudo ls > NUL
+# if [ $? -ne 0 ]; then
+#     read
+# fi
 
 # download catpic
 printf $green_text 'Downloading project...'
@@ -44,9 +65,15 @@ source ./venv/bin/activate
 # check if virtual environment activation successful
 if [ $(which python) = "/data/data/com.termux/files/home/catpic/venv/bin/python" ]
 then
-    # isntall Pillow dependency since it needs special treatment
+    # install Pillow dependency since it needs special treatment
     printf $green_text 'Installing Pillow dependency...'
-    LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
+    export LDFLAGS='-L/data/data/com.termux/files/usr/lib/'
+    export CFLAGS='-I/data/data/com.termux/files/usr/include/'
+    pip install Pillow
+else
+    printf $red_text 'Virtual environment activation failed, current python executable:'
+    printf $red_text $(which python)
+    printf $red_text 'Please restart.'
 fi
 
 printf $green_text 'Done.'
