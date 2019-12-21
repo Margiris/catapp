20dae3dedd4d87461499288883ea5ac57301abe3
Margiris <34870816+Margiris@users.noreply.github.com>
2019-12-22 00:08:52 +0200

    change port to make room for front-end

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from main import app
 from cheroot.wsgi import Server as WSGIServer, PathInfoDispatcher
 
 d = PathInfoDispatcher({'/': app})
-server = WSGIServer(('0.0.0.0', 80), d)
+server = WSGIServer(('0.0.0.0', 5000), d)
 
 if __name__ == '__main__':
     try:
